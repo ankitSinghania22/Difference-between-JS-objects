06f936811f6b21a39f89de4d394ac57bf427e9d3
ankitSinghania22 <87817068+ankitSinghania22@users.noreply.github.com>
2022-09-13 13:38:10 +0530

    Function Returns the Difference

---
+++ Difference.js
@@ -0,0 +1,15 @@
+import isEqual from 'lodash/isEqual';
+
+function getObjectDiff(obj1, obj2) {
+    const diff = Object.keys(obj1).reduce((result, key) => {
+        if (!obj2.hasOwnProperty(key)) {
+            result.push(key);
+        } else if (_.isEqual(obj1[key], obj2[key])) {
+            const resultKeyIndex = result.indexOf(key);
+            result.splice(resultKeyIndex, 1);
+        }
+        return result;
+    }, Object.keys(obj2));
+
+    return diff;
+}
